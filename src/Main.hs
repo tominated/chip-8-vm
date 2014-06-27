72aa9d16eccc5580d038300089f6b458cccce167
Tom Brunoli <tominated@gmail.com>
2014-06-27 22:46:19 +1000

    Remove console stepping.

--- src/Main.hs
+++ src/Main.hs
@@ -23,46 +23,6 @@ step s@VMState { pc = pc, memory = memory } =
     op = nextInstruction s
     s' = s { pc = pc + 2 }
 
--- | Steps through a program for each input
-stepLoop :: VMState -> IO ()
-stepLoop s@VMState { pc = pc, v = v, i = i, stack = stack} = do
-    putStr "PC: "
-    putStrLn $ showHex pc ""
-
-    putStr "Instruction: "
-    putStrLn $ showHex (nextInstruction s) ""
-
-    putStr "V: "
-    print v
-
-    putStr "I: "
-    print i
-
-    putStr "Stack: "
-    print stack
-
-    putStr $ showDisplay s
-
-    putStr "Press enter to step"
-    hFlush stdout
-    getLine
-    stepLoop $ step s
-
--- | Runs a program until it loops indefinitely
-run :: VMState -> IO ()
-run s@VMState { pc = pc } = do
-    -- Print the current display
-    putStr $ showDisplay s
-    hFlush stdout
-
-    -- Run the next instruction
-    let s'@VMState { pc = pc' } = step s
-
-    -- If the program counter hasn't changed, then
-    if pc == pc'
-    then return ()
-    else run s'
-
 drawScreen :: VMState -> Picture
 drawScreen s@VMState { display = d } =
     scale 1 (-1) $
